a38e9c2b55591588089ef2801fe9e5f1780936e8
Juan Manuel Barreda Fafasuli <juanmanuelbf@gmail.com>
2021-07-26 20:40:36 -0300

    Arrancando

--- HerramientasDeMaite/src/app/app-routing.module.ts
+++ HerramientasDeMaite/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseComponent } from './components/base/base.component';
+import { FaltantesEnListaComponent } from './components/faltantes-en-lista/faltantes-en-lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseComponent,
+    children: [
+      { path: '', component: FaltantesEnListaComponent },
+    ]
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
